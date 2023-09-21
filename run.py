2b27cb420460b0da2677b9fe80efe48d6bb25e09
tanhui <tanhui@zju.edu.cn>
2023-09-21 15:57:14 +0800

    up

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ from run.run_part1 import *
 def iso():
     
     save_path = path / "data" / "PART1"
-    case = list(range(1,4))
+    case = list(range(3,4))
     run(save_path, [*case], Part1, solve_model)
     
 def niso():
     save_path = path / "data" / "NPART1"
-    case = list(range(1,16))
+    case = list(range(3,4))
     run(save_path, [*case], Part1, Nsolve_model)
       
 
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     iso()
     
     # run non-iso party
-    #niso()
+    niso()
     
     
     
